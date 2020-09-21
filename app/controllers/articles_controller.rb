e489c1dbd3c5a442b35e64d90cd70e8a1bf1bbd2
solbencas <cecibenitezca@gmail.com>
2020-09-21 17:00:36 -0400

    Add flash messages to the app.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,12 +8,16 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
+    @article = Article.new
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save 
-    redirect_to @article
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render 'new'
+    end 
   end
 end
