20426d17b60b7a151e44b03c36440aa633a41606
solbencas <cecibenitezca@gmail.com>
2020-09-21 16:32:17 -0400

    form can now post articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,8 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save 
+    redirect_to @article
   end
 end
