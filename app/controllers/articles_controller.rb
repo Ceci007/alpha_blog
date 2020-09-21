964bc2bee41fe411b450e3230954de6c93acf228
solbencas <cecibenitezca@gmail.com>
2020-09-21 18:12:49 -0400

    Add links to all actions and pages.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,4 +34,10 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy()
+    redirect_to articles_path
+  end
 end
