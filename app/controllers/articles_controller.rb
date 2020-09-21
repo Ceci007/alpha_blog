a3ee58c552dbc64838e811215d793982ec5ab5a1
solbencas <cecibenitezca@gmail.com>
2020-09-21 17:25:05 -0400

    Add update functionality and add edit view.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit 
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
@@ -19,5 +23,15 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end 
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 end
