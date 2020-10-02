ca176d41b335543a5d68aef714a9b0bfd25dec9f
solbencas <cecibenitezca@gmail.com>
2020-10-02 13:10:00 -0400

    Add pagination to articles/index, users/index and users/show.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   def show() end
 
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def new
