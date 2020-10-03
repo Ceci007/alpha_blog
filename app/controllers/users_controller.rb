a81f11f50382fb5be392868f93a9b4b45d277d22
solbencas <cecibenitezca@gmail.com>
2020-10-03 11:19:21 -0400

    Add restriction to controllers and destroy method to users_controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update]
+  before_action :set_user, only: %i[show edit update destroy]
+  before_action :require_user, only: %i[edit update]
+  before_action :require_same_user, only: %i[edit update destroy]
 
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -36,6 +38,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles successfully deleted."
+    redirect_to articles_path
+  end
+
   private
 
   def user_params
@@ -44,5 +53,12 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user 
+    if current_user != @user
+      flash[:alert] = "You can only edit your own account."
+      redirect_to @user
+    end
   end
 end
