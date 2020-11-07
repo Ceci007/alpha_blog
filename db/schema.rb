e2ec9875152dfbec34ddab90e14ae965a24a3150
solbencas <cecibenitezca@gmail.com>
2020-11-07 10:12:22 -0300

    Add many to many association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_07_124043) do
+ActiveRecord::Schema.define(version: 2020_11_07_130819) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
