efbecce588e31ac971f01f3b08163a4f58abcd27
solbencas <cecibenitezca@gmail.com>
2020-11-07 10:06:00 -0300

    Add link to navigation, modify controller and change form styling

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
 
   def new
     @category = Category.new
@@ -15,7 +16,7 @@ class CategoriesController < ApplicationController
   end
 
   def index
-
+    @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def show
@@ -26,5 +27,12 @@ class CategoriesController < ApplicationController
 
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "Only admins can perform that action"
+      redirect_to categories_path
+    end
   end
 end
