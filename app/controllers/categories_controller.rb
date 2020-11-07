14483fad6c7883cb5dc6f07d621ea15fe8f2d847
solbencas <cecibenitezca@gmail.com>
2020-11-07 11:04:16 -0300

    finishing category touches

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,12 +15,27 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name updated successfully"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def show
-
+    @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
   
   private
